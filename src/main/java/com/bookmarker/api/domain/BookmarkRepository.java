998689698030fc6744793b5701c20fa06ac338a1
bahkha <haramx@naver.com>
2025-02-19 17:48:44 +0900

    f

--- src/main/java/com/bookmarker/api/domain/BookmarkRepository.java
+++ src/main/java/com/bookmarker/api/domain/BookmarkRepository.java
@@ -14,4 +14,16 @@ public interface BookmarkRepository extends JpaRepository<Bookmark, Long> {
 //    select new BookmarkDTO(b.id, b.title, b.url, b.createdAt) from Bookmark b
 //    """)
     Page<BookmarkDTO> findBookmarks(Pageable pageable);
+
+    @Query("""
+    select new com.bookmarker.api.dto.BookmarkDTO(b.id, b.title, b.url, b.createdAt) from Bookmark b
+    where lower(b.title) like lower(concat('%', :query, '%'))
+    """)
+    Page<BookmarkDTO> searchBookmarks(String query, Pageable pageable);
+
+    //method먕의 naming 규칙에 따라서 query문을 자동으로 생성해주는 query mqthod
+    //contains : % %, IgonoreCase: 대소문자를 구문안함
+    Page<BookmarkDTO> findByTitleContainsIgnoreCase(String query, Pageable pageable);
+
+
 }
--- src/main/java/com/bookmarker/api/service/BookmarkService.java
+++ src/main/java/com/bookmarker/api/service/BookmarkService.java
@@ -40,6 +40,16 @@ public class BookmarkService {
                 .map(mapper::toDTO);
         Page<BookmarkDTO> bookmark2Page = repository.findBookmarks(pageable);
         return new BookmarksDTO(bookmark2Page);
+
+    }
+
+    @Transactional(readOnly = true)
+    public BookmarksDTO searchBookmarks(String query, Integer page) {
+        int pageNo = page < 1 ? 0 : page - 1 ;
+        Pageable pageable = PageRequest.of(pageNo, 10, Sort.Direction.DESC, "createdAt");
+        //Page<BookmarkDTO> bookmarkPage = repository.searchBookmarks(query, pageable);
+        Page<BookmarkDTO> bookmarkPage = repository.findByTitleContainsIgnoreCase(query, pageable);
+        return new BookmarksDTO(bookmarkPage);
     }
 
 }
