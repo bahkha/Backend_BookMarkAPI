22001a12c79a5885b63f3677d73d050d82f2cdd1
bahkha <haramx@naver.com>
2025-02-19 17:28:59 +0900

    search

--- src/main/java/com/bookmarker/api/service/BookmarkService.java
+++ src/main/java/com/bookmarker/api/service/BookmarkService.java
@@ -2,7 +2,14 @@ package com.bookmarker.api.service;
 
 import com.bookmarker.api.domain.Bookmark;
 import com.bookmarker.api.domain.BookmarkRepository;
+import com.bookmarker.api.dto.BookmarkDTO;
+import com.bookmarker.api.dto.BookmarkMapper;
+import com.bookmarker.api.dto.BookmarksDTO;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -13,6 +20,7 @@ import java.util.List;
 @RequiredArgsConstructor //lombok이 injection
 public class BookmarkService {
     private final BookmarkRepository repository;
+    private final BookmarkMapper mapper;
 
     //Constructor Injection
     //@RequiredArgsConstructor 대신에 직접 생성자 선언
@@ -21,8 +29,17 @@ public class BookmarkService {
 //    }
 
     @Transactional(readOnly = true)
-    public List<Bookmark> getBookmarks() {
-        return repository.findAll();
+    public BookmarksDTO getBookmarks(Integer page) {
+        //JPA의 페이지번호가 0부터 시작하기때문
+        int pageNo = page < 1 ? 0 : page - 1;
+        Pageable pageable = PageRequest.of(pageNo, 10, Sort.Direction.DESC, "id");
+        Page<BookmarkDTO> bookmarkPage = repository.findAll(pageable)
+        //map(Function) Function의 추상메소드 R apply(T t)
+        // .map(bookmark -> mapper.toDTO(bookmark));
+        //Method Refrence
+                .map(mapper::toDTO);
+        Page<BookmarkDTO> bookmark2Page = repository.findBookmarks(pageable);
+        return new BookmarksDTO(bookmark2Page);
     }
-    
+
 }
